056c5e8f17b88af8c6e2c986e3aebdef393a092c
Hardik <hardik.ats.expert@gmail.com>
2023-05-03 16:31:28 +0530

    Add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,9 @@ User.create!(name: "hardik",
             email: "hardik@example.com",
             password: "147852", 
             password_confirmation: "147852", 
-            admin: true)
+            admin: true,
+            activated: true,
+            activated_at: Time.zone.now)
 
 # Generate a bunch of additional users.
 30.times do |n|
@@ -12,6 +14,8 @@ User.create!(name: "hardik",
     User.create!(name: name,
                 email: email,
                 password: password,
-                password_confirmation: password)
+                password_confirmation: password,
+                activated: true,
+                activated_at: Time.zone.now)
     
 end
